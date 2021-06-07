f8fbd504866460b5ad2379257060ecfcd21f22c8
vladimirkachan <vladimirkachan0@gmail.com>
2021-06-07 23:43:35 +0300

    01 project created

---
+++ 01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (var db = new Context())
+            //{
+            //    Console.WriteLine("--- FromSql: #1 ---");
+            //    Console.ReadKey();
+
+            //    var companies = db.Companies.FromSql("SELECT * FROM Companies").ToList();
+            //    foreach (var company in companies)
+            //    {
+            //        Console.WriteLine(company.Name);
+            //    }
+
+            //    Console.WriteLine("--- FromSql: #2 ---");
+            //    Console.ReadKey();
+            //    var companies2 = db.Companies.FromSql("SELECT * FROM Companies").OrderBy(x => x.Name).ToList();
+            //    foreach (var company in companies)
+            //    {
+            //        Console.WriteLine(company.Name);
+            //    }
+
+            //    Console.WriteLine("--- Finish ---");
+            //    Console.ReadKey();
+            //}
+        }
+    }
+}
