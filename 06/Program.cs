d0151fd66be28d9a3bda93c09b06b55972a51f69
vladimirkachan <vladimirkachan0@gmail.com>
2021-06-08 11:31:16 +0300

    06 project - StoredProcedures

---
+++ 06/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using _01;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace _06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using Context db = new ();
+            Console.WriteLine("--- Stored Procedures ---");
+
+            SqlParameter param1 = new ("@name", "Samsung");
+            var phones = db.Phones.FromSqlRaw("GetPhonesByCompany @name", param1).ToList();
+            foreach (var p in phones) Console.WriteLine($"{p.Name} - {p.Price}");
+            Console.WriteLine("--- OUTPUT ---");
+
+            var param2 = new SqlParameter
+            {
+                ParameterName = "@phoneName",
+                SqlDbType = SqlDbType.VarChar,
+                Direction = ParameterDirection.Output,
+                Size = 50
+            };
+            db.Database.ExecuteSqlRaw("GetPhoneWithMaxPrice @phoneName OUT", param2);
+            Console.WriteLine(param2.Value);
+            Console.ReadKey();
+        }
+    }
+}
