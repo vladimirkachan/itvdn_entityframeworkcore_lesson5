05faafab2bc7172c64a2691932a4f393069a422d
vladimirkachan <vladimirkachan0@gmail.com>
2021-06-08 10:47:49 +0300

    05 project - StrorageFunction

---
+++ 05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using _01;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace _05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using Context db = new Context();
+            SqlParameter param = new ("@price", 50000);
+            var phones = db.Phones.FromSqlRaw("SELECT * FROM GetPhonesByPrice (@price)", param).ToList();
+            foreach (var p in phones)
+                Console.WriteLine($"{p.Name} - {p.Price}");
+            Console.ReadKey();
+        }
+    }
+}
