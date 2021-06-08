4f6e215f523daadfe8becb82cffddf5e87fa1131
vladimirkachan <vladimirkachan0@gmail.com>
2021-06-08 08:45:20 +0300

    02 project - SqlParameter

---
+++ 02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using _01;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using Context db = new ();
+            Console.WriteLine("--- SqlParameter: #1 ---");
+
+            SqlParameter param = new ("@name", "%Sony%");
+            var phones1 = db.Phones.FromSqlRaw("SELECT * FROM Phones WHERE Name LIKE @name", param).ToList();
+            foreach (var phone in phones1) Console.WriteLine(phone.Name);
+            Console.WriteLine("--- SqlParameter: #2 ---");
+
+            var name = "%Galaxy%";
+            var phones2 = db.Phones.FromSqlRaw("SELECT * FROM Phones WHERE Name LIKE {0}", name).ToList();
+            foreach (var phone in phones2) Console.WriteLine(phone.Name);
+            Console.WriteLine("--- SqlParameter: #3 ---");
+
+            var price = 30000;
+            var phones3 = db.Phones.FromSqlRaw("SELECT * FROM Phones WHERE Price > {0}", price).ToList();
+            foreach (var phone in phones3) Console.WriteLine(phone.Name);
+            Console.WriteLine("--- Finish ---");
+            Console.ReadKey();
+        }
+    }
+}
