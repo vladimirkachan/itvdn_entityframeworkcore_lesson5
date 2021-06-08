8ab2426ef63e594d05e4613be84bda55fbc96e31
vladimirkachan <vladimirkachan0@gmail.com>
2021-06-08 08:19:52 +0300

    01 project - FromSqlRaw

--- 01/Program.cs
+++ 01/Program.cs
@@ -12,15 +12,14 @@ namespace _01
             using var db = new Context();
             //// Generate(db);
             Console.WriteLine("--- FromSql: #1 ---");
-            Console.ReadKey();
 
             var companies = db.Companies.FromSqlRaw("SELECT * FROM Companies").ToList();
             foreach (var company in companies) Console.WriteLine(company.Name);
             Console.WriteLine("--- FromSql: #2 ---");
-            Console.ReadKey();
-            var companies2 = db.Companies.FromSqlRaw("SELECT * FROM Companies").OrderBy(x => x.Name).ToList();
-            foreach (var company in companies) Console.WriteLine(company.Name);
-            Console.WriteLine("--- Finish ---");
+
+            var companies2 = db.Companies.FromSqlRaw("SELECT * FROM Companies").OrderByDescending(x => x.Name).ToList();
+            foreach (var company in companies2) Console.WriteLine(company.Name);
+
             Console.ReadKey();
         }
         static void Generate(Context db)
